62b9b503f7e5a9d11b7959b9245e510231e43d40
frahohen <f.hohenb@gmail.com>
2021-05-01 22:19:46 +0200

    Improve logging of setup.sh and install_webmin.sh

--- install_webmin/install_webmin.sh
+++ install_webmin/install_webmin.sh
@@ -5,8 +5,6 @@ LOGGER="./utils/print_log.sh"
 REPO_WEBMIN="deb https://download.webmin.com/download/repository sarge contrib"
 FILE_SOURCE_LIST="/etc/apt/sources.list"
 
-bash "${LOGGER}" info "Execute webmin installation"
-
 if grep -Fxq "${REPO_WEBMIN}" "${FILE_SOURCE_LIST}"
 then
 	bash "${LOGGER}" info "Repository \"${REPO_WEBMIN}\" does already exist in file \"${FILE_SOURCE_LIST}\""
@@ -17,15 +15,23 @@ else
 	echo "${REPO_WEBMIN}" >> "${FILE_SOURCE_LIST}"
 fi
 
-bash "${LOGGER}" info "Installing GPG key"
+bash "${LOGGER}" info "Install gnupg1"
 apt-get install -y gnupg1
+
+bash "${LOGGER}" info "Install GPG key"
 cd /root
 wget https://download.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
 
-bash "${LOGGER_UTIL}" info "Update and install webmin"
+bash "${LOGGER}" info "Install apt-transport-https"
 apt-get install -y apt-transport-https
+
+bash "${LOGGER}" info "Clean all packages"
 apt-get clean all
+bash "${LOGGER}" info "Re-index and fetch all available packages"
 apt-get update -y
+bash "${LOGGER}" info "Upgrade all packages to the latest version available"
 apt-get upgrade -y
+
+bash "${LOGGER_UTIL}" info "Install webmin"
 apt-get install -y webmin
--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 # General
 LOGGER="./utils/print_log.sh"
 
-bash "${LOGGER}" info "Enter root user password to execute webmin installation"
+bash "${LOGGER}" info "Execute environment setup"
 su root ./install_webmin/install_webmin.sh 
+bash "${LOGGER}" info "Environment setup complete"
