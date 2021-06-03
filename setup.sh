0721759503d9910773ef5f62bd7ffbcbf3c9cfcd
frahohen <f.hohenb@gmail.com>
2021-06-03 14:11:44 +0200

    Add install_docker_compose.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ LOGGER="./utils/print_log.sh"
 bash "${LOGGER}" info "Execute environment setup"
 su root ./install_webmin/install_webmin.sh 
 su root ./install_docker/install_docker.sh 
+su root ./install_docker_compose/install_docker_compose.sh 
 bash "${LOGGER}" info "Environment setup complete"
